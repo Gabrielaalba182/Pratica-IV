c5a44270387472f2fbfb096135c7b431335245b4
Gabrielaalba182 <gabrielaalba75@gmail.com>
2025-11-12 14:24:52 -0300

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,3 @@
 export default async function page() {
-  return `<h1>Bem-vindo</h1><p>Este é o app dinâmico da Prática III. Use o menu para navegar.</p>`;
+  return `<h1>Bem-vindo</h1><p>Este é o app dinâmico da Prática IV. Use o menu para navegar.</p>`;
 }
